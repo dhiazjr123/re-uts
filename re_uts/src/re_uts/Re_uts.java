c45b812832c494f8950bbaca55becf2ad1d7c3f3
dhiazjr123 <118143683+dhiazjr123@users.noreply.github.com>
2023-11-20 13:08:17 +0700

    Add files via upload

---
+++ re_uts/src/re_uts/Re_uts.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package re_uts;
+
+/**
+ *
+ * @author DELL
+ */
+public class Re_uts {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
